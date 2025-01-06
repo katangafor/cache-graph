7c0baaf91ac18463995ded72e4b6693e637d9ef0
Johnny Hane <johnnykhane@gmail.com>
2025-01-06 15:10:02 -0500

    looks non-recursive function node is working with cacheables

--- src/graph.ts
+++ src/graph.ts
@@ -44,6 +44,104 @@ export const genCachify = <TArgs extends unknown[], TReturn>({
   };
 };
 
+// trying to make a non-recursive version to share on SO for help
+// part of the issue is how to actually format the parent functions' args?
+// like if you have two parent functions, are they all just in a tuple?
+// are they smushed together into the same args list? (no, how would you combine objects and regular params)
+// I feel like maybe you have to provide the name of the function as an obj property, then the arguments array as its value
+
+// so an MVP for this type would be:
+// you have two properties: a function called "fn" and a list (tuple or object? maybe try both) of parent functions called "parentFns"
+
+// ok first we need to see if TS can infer the types of multiple things in an array at once
+
+// ok so you can infer a whole union of stuff. So it must be possible to infer the function arguments of an object full of functions??
+type funcObj = {
+  [key: string]: (...args: any) => any;
+};
+
+type funcObjArgs<T extends funcObj> = {
+  [K in keyof T]: Parameters<T[K]>;
+};
+
+type dumbNode<T extends funcObj> = {
+  parentFns: T;
+  fn: (parentFnArgs: funcObjArgs<T>) => any;
+};
+
+const makeFunctionNode = <T extends funcObj>(dumbNode: dumbNode<T>) => dumbNode;
+
+const exampleFuncs = {
+  getName: (name: string, lastName: string) => name,
+  getDoubleAge: (age: number, multiplier: number) => age * 2,
+};
+
+const myDumbFuncNode = makeFunctionNode({
+  parentFns: exampleFuncs,
+  fn: (parentFuncSigs) => {
+    // just giving em underscores to differentiate them from actual ones
+    const { getDoubleAge: getDoubleAgeSig, getName: getNameSig } = parentFuncSigs;
+    // I should be able to call those functions with the params I have in my tuples
+    const doubleAge = exampleFuncs.getDoubleAge(...getDoubleAgeSig);
+    //    ^?
+    const name = exampleFuncs.getName(...getNameSig);
+    //    ^?
+  },
+});
+
+// we'll use these instead of functions in funcNodes. I don't think we'll have any raw functions? just these babies.
+// OK I can't make the cacheable example work 1-to-1 with the dumbFuncNode since cacheable is generic, so imma
+// remove the generic restriction FOR NOW to see what I can do
+type cacheable = {
+  fn: (...args: any) => any;
+  genKey: (...args: any) => string;
+};
+
+type cacheableObj = {
+  [key: string]: cacheable;
+};
+
+// difference between funcs and cacheables is that while the functions can be anything,
+// the cacheable for fn needs to get its args from parentFns
+type cacheableFnArg<T extends cacheableObj> = {
+  [K in keyof T]: Parameters<T[K]["fn"]>;
+};
+
+type cacheableFnNode<T extends cacheableObj> = {
+  parentFns: T;
+  fn: (parentFnArgs: cacheableFnArg<T>) => any;
+};
+
+const makeCacheableFnNode = <T extends cacheableObj>(
+  cacheableFnNode: cacheableFnNode<T>,
+) => cacheableFnNode;
+
+const exampleCacheables = {
+  getName: {
+    fn: (name: string, lastName: string) => name,
+    genKey: (name: string, lastName: string) => `name:${name}:${lastName}`,
+  },
+  getDoubleAge: {
+    fn: (age: number, multiplier: number) => age * 2,
+    genKey: (age: number, multiplier: number) => `age:${age}:${multiplier}`,
+  },
+};
+
+const myCacheableFuncNode = makeCacheableFnNode({
+  parentFns: exampleCacheables,
+  fn: (parentFuncSigs) => {
+    const { getDoubleAge: getDoubleAgeSig, getName: getNameSig } = parentFuncSigs;
+    const doubleAge = exampleCacheables.getDoubleAge.fn(...getDoubleAgeSig);
+    const name = exampleCacheables.getName.fn(...getNameSig);
+  },
+});
+
+// ok I can infer an object's worth of function arguments, and they're associated to the function by name.
+// I think that the function nodes will need a keygen function specificied explicitly: a cachified function
+// has the exact same signature as the func itself, just with extra side effects. So you can't figure out
+// how to generate a cache key from the cachified function. The genkey needs to be provided so that children
+// can correctly generate the cache keys for their parents.
+
 // ## FUNCTION NODES ##
 // each function node will have a cachified function, a list of parent nodes.
 // when you call a function, you will also have to provide the args of the parent nodes'
@@ -53,12 +151,6 @@ export const genCachify = <TArgs extends unknown[], TReturn>({
 // children are just any other nodes
 // if you actually want to infer the correct types of the whole tree for whatever reason,
 // you'll probs have to use mapped types with infer (to avoid passing any to the children generic)
-
-// ACTUALLY i=
-type FancyFunctionNode<TArgs extends unknown[], TReturn> = {
-  func: typeof withCache;
-  children?: FancyFunctionNode<any, any>[];
-};
 
 // https://stackoverflow.com/questions/72370457/generic-type-for-args
 
@@ -123,66 +215,27 @@ type FancyFunctionNode<TArgs extends unknown[], TReturn> = {
 // cause then you can recursively loop through parents, making sure that all of the args have been
 // provided.
 
-// trying to make a non-recursive version to share on SO for help
-// part of the issue is how to actually format the parent functions' args?
-// like if you have two parent functions, are they all just in a tuple?
-// are they smushed together into the same args list? (no, how would you combine objects and regular params)
-// I feel like maybe you have to provide the name of the function as an obj property, then the arguments array as its value
+// Jan 6th
+// so I have that dumbFuncNode. It forces fn to provide all of the necessary args required by each function in parentFns.
+// But nobody wants to use that. So we have genCachify, which lets you curry the get/set in early, so all that's left is
 
-// so an MVP for this type would be:
-// you have two properties: a function called "fn" and a list (tuple or object? maybe try both) of parent functions called "parentFns"
+// BRAINSTORM what's the deal with caching types?
+// withCache takes a function, genKey function, AND func args (along with get/set cache methods).
+// genCachify lets you curry the get/set funcs early, and RETURNS a cachify function.
+// a cachify function is one that takes a fetchFn and a genKey.
 
-type FunctionNode<T> = {
-  // parentFns is a tuple of functions?? and somehow the type of each on is inferred?
-  // parentFns:
-};
+// in retrospect, that is indeed exactly what we need to provide to the function nodes.
+// you won't be providing a cachified function, you'll be providing the same args that cachify takes:
+// a fetchFn and a genKey
 
-// ok first we need to see if TS can infer the types of multiple things in an array at once
-type typedTuple<T extends Function[]> = T;
-const myFuncs = [(name: string) => 5, (age: number) => {}];
+// so how do we get from a dumbFuncNode to one using genKey n shit?
+// do I actually need to get a full withCache function working to try out these types?
+// I don't THINK I do: the guts of a withCache function doesn't really matter yet.
+// As long as function nodes have a fn and genKey, I can get the types working. You wouldn't actually
+// be caching anything without the withCache usage, but I'm not hooking the caching/prefixing up
+// yet anyway.
 
-type UnionOfArgs<T extends readonly Function[]> = T[number] extends (...args: any) => any
-  ? Parameters<T[number]>[number]
-  : never;
+// I'll start by trying to define a new type: an object (called cacheable?) that has both a fn and a genKey (which obvi take the same args).
+// Then I'll need to get a funcNode working that only has cacheables, instead of functions.
 
-type argUnion = UnionOfArgs<typeof myFuncs>;
-
-// ok so you can infer a whole union of stuff. So it must be possible to infer the function arguments of an object full of functions??
-type funcObj = {
-  [key: string]: (...args: any) => any;
-};
-
-type funcObjArgs<T extends funcObj> = {
-  [K in keyof T]: Parameters<T[K]>;
-};
-
-type dumbNode<T extends funcObj> = {
-  parentFns: T;
-  fn: (parentFnArgs: funcObjArgs<T>) => any;
-};
-
-const makeFunctionNode = <T extends funcObj>(dumbNode: dumbNode<T>) => dumbNode;
-
-const exampleFuncs = {
-  getName: (name: string, lastName: string) => name,
-  getDoubleAge: (age: number, multiplier: number) => age * 2,
-};
-
-const myDumbFuncNode = makeFunctionNode({
-  parentFns: exampleFuncs,
-  fn: (parentFuncSigs) => {
-    // just giving em underscores to differentiate them from actual ones
-    const { getDoubleAge: getDoubleAgeSig, getName: getNameSig } = parentFuncSigs;
-    // I should be able to call those functions with the params I have in my tuples
-    const doubleAge = exampleFuncs.getDoubleAge(...getDoubleAgeSig);
-    //    ^?
-    const name = exampleFuncs.getName(...getNameSig);
-    //    ^?
-  },
-});
-
-// ok I can infer an object's worth of function arguments, and they're associated to the function by name.
-// I think that the function nodes will need a keygen function specificied explicitly: a cachified function
-// has the exact same signature as the func itself, just with extra side effects. So you can't figure out
-// how to generate a cache key from the cachified function. The genkey needs to be provided so that children
-// can correctly generate the cache keys for their parents.
+// NOTE the idea might be easier to explain using dumbFuncNode, so keep it around for SO posts n such
